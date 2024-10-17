becf7b239cb8a197750c9eb8a52e0f2885793980
Wagner Bruna <wbruna@users.noreply.github.com>
2024-10-16 23:37:54 -0300

    Embed generation data into the generated images

--- src/backend/image_saver.py
+++ src/backend/image_saver.py
@@ -4,6 +4,7 @@ from typing import Any
 from uuid import uuid4
 from backend.models.lcmdiffusion_setting import LCMDiffusionSetting
 from utils import get_image_file_extension
+from PIL import PngImagePlugin
 
 
 def get_exclude_keys():
@@ -56,7 +57,30 @@ class ImageSaver:
                 if not path.exists(out_path):
                     mkdir(out_path)
                 image_extension = get_image_file_extension(format)
-                image.save(path.join(out_path, f"{gen_id}-{index+1}{image_extension}"))
+
+                meta_args = {}
+
+                # Save metadata as Gradio does:
+                # - png metadata collecting any string inside info
+                # - exif already in serialized formatted
+
+                if format == "PNG":
+                    info = PngImagePlugin.PngInfo()
+                    have_meta = False
+                    for key, value in image.info.items():
+                        if isinstance(key, str) and isinstance(value, str):
+                            info.add_text(key, value)
+                            have_meta = True
+                    if have_meta:
+                        meta_args["pnginfo"] = info
+
+                if format == 'JPEG':
+                    exif = image.info.get("exif")
+                    if exif:
+                        meta_args["exif"] = exif
+
+                image.save(path.join(out_path, f"{gen_id}-{index+1}{image_extension}"), **meta_args)
+
             if lcm_diffusion_setting:
                 data = lcm_diffusion_setting.model_dump(exclude=get_exclude_keys())
                 if image_seeds:
